6c0dea80e58c668dae304f5327ec35c76df61fa6
SlobodyanSN <110773219+SlobodyanSN@users.noreply.github.com>
2023-02-25 18:45:10 +0200

    1-st_task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const btnStart = document.querySelector(`button[data-start]`);
+const btnStop = document.querySelector(`button[data-stop]`);
+const bgn = document.querySelector(`body`);
 
+btnStop.disabled = true;
+
+let timerId = null;
+
+changeBgn();
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function changeBgn() {bgn.style.background = getRandomHexColor()};
+
+btnStart.addEventListener(`click`, (onStartBtnClick));
+
+function onStartBtnClick () {
+btnStart.disabled = true;
+btnStop.disabled = false;
+
+timerId = setInterval(changeBgn, 1000);
+
+};
+
+btnStop.addEventListener(`click`, (onStopBtnClick));
+
+function onStopBtnClick() {
+clearInterval(timerId)
+
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+};
